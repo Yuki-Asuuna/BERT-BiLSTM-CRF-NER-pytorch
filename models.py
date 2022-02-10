68369bcad06512a50c66c756a090111e155c70ed
sunyuhong <sunyuhong@bytedance.com>
2022-02-10 14:48:23 +0800

    feat: add attention
    Change-Id: Iae7bc57c59c4399a6f0d7f1100ac24d4c171cbdc

--- models.py
+++ models.py
@@ -6,6 +6,66 @@ from torchcrf import CRF
 from pytorch_transformers import BertPreTrainedModel, BertModel
 from utils import mfcc39
 import numpy as np
+from typing import Tuple
+
+
+# class AdditiveAttention(nn.Module):
+#     """
+#      Applies a additive attention (bahdanau) mechanism on the output features from the decoder.
+#      Additive attention proposed in "Neural Machine Translation by Jointly Learning to Align and Translate" paper.
+#      Args:
+#          hidden_dim (int): dimesion of hidden state vector
+#      Inputs: query, value
+#          - **query** (batch_size, q_len, hidden_dim): tensor containing the output features from the decoder.
+#          - **value** (batch_size, v_len, hidden_dim): tensor containing features of the encoded input sequence.
+#      Returns: context, attn
+#          - **context**: tensor containing the context vector from attention mechanism.
+#          - **attn**: tensor containing the alignment from the encoder outputs.
+#      Reference:
+#          - **Neural Machine Translation by Jointly Learning to Align and Translate**: https://arxiv.org/abs/1409.0473
+#     """
+#
+#     def __init__(self, hidden_dim: int) -> None:
+#         super(AdditiveAttention, self).__init__()
+#         self.query_proj = nn.Linear(hidden_dim, hidden_dim, bias=False)
+#         self.key_proj = nn.Linear(hidden_dim, hidden_dim, bias=False)
+#         self.bias = nn.Parameter(torch.rand(hidden_dim).uniform_(-0.1, 0.1))
+#         self.score_proj = nn.Linear(hidden_dim, 1)
+#
+#     def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+#         score = self.score_proj(torch.tanh(self.key_proj(key) + self.query_proj(query) + self.bias)).squeeze(-1)
+#         attn = F.softmax(score, dim=-1)
+#         context = torch.bmm(attn.unsqueeze(1), value)
+#         return context, attn
+
+# Additive-Attention(q,k) = W_v * tanh( W_q * q + W_k * k )
+class AdditiveAttention(nn.Module):
+    def __init__(self, key_size, query_size, num_hiddens, **kwargs):  # 转换为num_hiddens维度，词向量长度
+        # 假设：query：(2, 1, 20), key：(2, 10, 2), value： (2, 10, 4)
+        # batch seq word_embedding，  key和value seq_len是一样的，query是一个单独的向量，1×20
+        super(AdditiveAttention, self).__init__(**kwargs)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
+        self.w_v = nn.Linear(num_hiddens, 1, bias=False)
+
+    # queries [batch_size,word_num,rnn_dim_text]
+    # keys [batch_size,frame_num,rnn_dim_audio]
+    # values [batch_size,frame_num,rnn_dim_audio]
+    def forward(self, queries, keys, values):
+        queries, keys = self.W_q(queries), self.W_k(keys)  # 映射到相同维度 [2,1,8] [2,10,8]
+        # query增加一个维度为了方便和key相加。key增加一个维度后面需要
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)  # torch.Size([2, 1, 1, 8]) torch.Size([2, 1, 10, 8])
+        # features = torch.Size([2, 1, 10, 8])
+        features = torch.tanh(features)
+
+        scores = self.w_v(features)  # 8 *1
+        # torch.Size([2, 1, 10, 1])
+        scores = scores.squeeze(-1)  # w_v消掉最后隐藏层维
+        # torch.Size([2, 1, 10]) [batch_size, word_num, frame_num]
+
+        self.attention_weigths = nn.functional.softmax(scores)
+        # attention weights和values加权相加
+        return torch.bmm(self.attention_weigths, values)  # 2,1,10 2*10*4 ->2*1*4,10个value的权重加和
 
 
 class BERT_BiLSTM_CRF(BertPreTrainedModel):
@@ -26,19 +86,17 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
             # hidden_size默认为768，bert字向量输出维度
             self.birnn_text = nn.LSTM(config.hidden_size, rnn_dim_text, num_layers=1, bidirectional=True,
                                       batch_first=True)
+            # rnn_dim_text 默认256
             out_dim = rnn_dim_text * 2
 
         # 是否需要加入音频
         if need_audio:
             # 语音LSTM层
             self.birnn_audio = nn.LSTM(39, rnn_dim_audio, num_layers=1, bidirectional=True, batch_first=True)
+            # rnn_dim_audio 默认 256
+
             # Attention Layer Definition
-            self.u = nn.Parameter(torch.Tensor(1, rnn_dim_audio * 2))
-            self.v = nn.Parameter(torch.Tensor(1, rnn_dim_text * 2))
-            self.b = nn.Parameter(torch.tensor(1.0))
-            # Initialize
-            nn.init.uniform_(self.u, -0.1, 0.1)
-            nn.init.uniform_(self.v, -0.1, 0.1)
+            self.attention_layer = AdditiveAttention(rnn_dim_text * 2, rnn_dim_audio * 2, rnn_dim_audio * 2)
             # Attention Layer Definition End
 
             # 多模态融合层
@@ -79,40 +137,23 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
         # 语音LSTM
         if self.need_audio:
             sequence_output_audio, _ = self.birnn_audio(audio_feat)
-            # a_j_i=tanh(u*s_i+v*h_j+b)
-            # s_i => (1,rnn_dim_audio*2),h_j => (1,rnn_dim_text*2)
-            batch_num = sequence_output_text.size(0)
-            frame_num = sequence_output_audio.size(1)
-            word_num = sequence_output_text.size(1)
-            alpha = torch.randn((word_num, frame_num))
-            for k in range(batch_num):
-                for j in range(word_num):
-                    for i in range(frame_num):
-                        alpha[j][i] = torch.mm(sequence_output_audio[k][i].unsqueeze(0), self.u.t()) + torch.mm(sequence_output_text[k][j].unsqueeze(0),self.v.t()) + self.b
 
-            alpha = torch.tanh(alpha)
-            alpha = nn.functional.softmax(alpha)
-
-            # 计算第j个字所对应的语音特征
-            new_h = torch.zeros(batch_num, word_num,sequence_output_audio.size(-1))
-            for k in range(batch_num):
-                for j in range(word_num):
-                    for i in range(frame_num):
-                        new_h[k][j] += alpha[j][i] * sequence_output_audio[k][i]
-
-            fuse = torch.cat((new_h,sequence_output_text),dim=-1)
-            fused_output,_ = self.birnn_fuse(fuse)
-
-            fused_output = self.dropout(fused_output)
-            emissions = self.hidden2tag(fused_output)
-
+        if self.need_audio:
+            attn = self.attention_layer(sequence_output_text, sequence_output_audio, sequence_output_audio)
+            fuse = torch.cat((sequence_output_text, attn), dim=-1)
+            sequence_output_fused, _ = self.birnn_fuse(fuse)
+            sequence_output_fused = self.dropout(sequence_output_fused)
+            emissions = self.hidden2tag(sequence_output_fused)
             return emissions
+
         else:
             sequence_output_text = self.dropout(sequence_output_text)
             emissions = self.hidden2tag(sequence_output_text)
             return emissions
 
     def predict(self, input_ids, token_type_ids=None, input_mask=None, audio_data=None):
-        audio_feat = self.audio_feat_extract(audio_data)
+        audio_feat = None
+        if self.need_audio:
+            audio_feat = self.audio_feat_extract(audio_data)
         emissions = self.tag_outputs(input_ids, token_type_ids, input_mask, audio_feat)
         return self.crf.decode(emissions, input_mask.byte())
